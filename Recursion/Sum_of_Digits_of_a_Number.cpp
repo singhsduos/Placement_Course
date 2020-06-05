3068336d7ee2f97c3bc7650edcc93938e278dc6c
Neelesh Singh <ns9628491678@gmail.com>
2020-06-05 20:28:59 +0530

    first commit

---
+++ Recursion/Sum_of_Digits_of_a_Number.cpp
@@ -0,0 +1,68 @@
+/*
+You are given a number n. You need to find the sum of digits of n.
+
+Input:
+The first line of input contains T denoting the number of testcases. T testcases follow. Each testcase contains one line of input containing n.
+
+Output:
+For each testcase, in a newline, print the sum of digits of n.
+
+Your Task:
+This is a function problem. You only need to complete the function sumOfDigits that takes n as parameter and returns the sum of digits of n.
+
+Constraints:
+1 <= T <= 105
+1 <= n <= 107
+
+Examples:
+Input:
+2
+1
+99999
+Output:
+1
+45
+
+Explanation:
+Testcase 1: Sum of digits for 1 is 1.
+Testcase 2: Sum of digits for 99999 is 9 + 9 + 9 + 9 + 9 = 45  
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Complete this function
+int sumOfDigits(int n)
+{
+   if(n==0)
+   return n;
+   else
+   return sumOfDigits(n/10)+(n%10);
+   
+}
+
+// { Driver Code Starts.
+
+
+int main() {
+	int T;
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    cout<<sumOfDigits(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
