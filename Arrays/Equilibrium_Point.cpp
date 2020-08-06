1553064278d5c520a3df5b1085ad053957341750
Neelesh Singh <ns9628491678@gmail.com>
2020-08-06 21:22:16 +0530

    first point

---
+++ Arrays/Equilibrium_Point.cpp
@@ -0,0 +1,85 @@
+/*
+ Given an array A of N positive numbers. The task is to find the first Equilibium Point in the array. 
+Equilibrium Point in an array is a position such that the sum of elements before it is equal to the sum of elements after it.
+
+Example 1:
+
+Input:
+N = 1
+A[] = {1}
+Output: 1
+Explanation: Since its the only 
+element hence its the only equilibrium 
+point. 
+Example 2:
+
+Input:
+N = 5
+A[] = {1,3,5,2,2}
+Output: 3
+Explanation: For second test case 
+equilibrium point is at position 3 
+as elements before it (1+3) = 
+elements after it (2+2).
+ 
+
+Your Task:
+The task is to complete the function equilibriumPoint() which takes the array and N as input parameters and returns the point of equilibrium. Return -1 if no such point exists.
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 <= N <= 106
+1 <= A[i] <= 108
+ */
+
+
+#include <iostream>
+using namespace std;
+
+// Position this line where user code will be pasted.
+
+int equilibriumPoint(long long a[], int n);
+
+int main() {
+
+    long long t;
+    cin >> t;
+
+    while (t--) {
+        long long n;
+        cin >> n;
+        long long a[n];
+
+        for (long long i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+
+        cout << equilibriumPoint(a, n) << endl;
+    }
+    return 0;
+}
+
+
+
+// Function to find equilibrium point
+// a: input array
+// n: size of array
+int equilibriumPoint(long long a[], int n) {
+    int sum=0,TotalSum=0;
+  for(int i=0;i<n;i++)
+  TotalSum+=a[i];
+  for(int i=0;i<n;i++){
+     
+    if(sum==(TotalSum-a[i])*0.5)
+    return i+1;
+    if(sum>(TotalSum-a[i])*0.5)
+    return -1;
+    sum+=a[i];
+      
+  }
+
+   
+}
+
